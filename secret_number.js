bbc4c2c49f0c93857e9718f804d1afa24554e96f
Crescendi <martahonolulu@hotmail.com>
2016-09-07 19:08:05 -1000

    secret number module done

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,13 @@
 'use strict';
 module.exports = function() {
-  
+
+  var secret_number = 0;
+  secret_number = Math.floor((Math.random() * 1000000));
+
+  function secretNumber(){
+    return secret_number;
+  }
+
+return secretNumber;
+
 };
